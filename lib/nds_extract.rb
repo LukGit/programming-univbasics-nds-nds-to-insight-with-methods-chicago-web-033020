9ebe8c0d85765da2e5d7535312b4c43947a047d3
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-24 23:38:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,6 +6,7 @@ require 'directors_database'
 def gross_for_director(director_data)
   counter2 = 0 
   grossdir = 0
+  pp director_data
   while counter2 < director_data[:movies].length do
     grossdir += director_data[counter2][:worldwide_gross]
     counter2 += 1 
@@ -18,14 +19,14 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-  result = {}
-  counter1 = 0 
-  while counter1 < nds.length do
-    name = nds[counter1][:name]
-    directorgr = gross_for_director(nds[counter1])
-    result[name] = directorgr
-    #pp result
-    counter1 += 1 
-  end
-  result
+  # result = {}
+  # counter1 = 0 
+  # while counter1 < nds.length do
+  #   name = nds[counter1][:name]
+  #   directorgr = gross_for_director(nds[counter1])
+  #   result[name] = directorgr
+  #   #pp result
+  #   counter1 += 1 
+  # end
+  # result
 end
