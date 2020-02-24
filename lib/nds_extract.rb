02c14cdd88a2b91937f31923fa75f7a4457b2622
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-24 23:26:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,7 @@ def directors_totals(nds)
     name = nds[counter1][:name]
     directorgr = gross_for_director(nds[counter1][:movies])
     result[name] = directorgr
-    pp result
+    #pp result
     counter1 += 1 
   end
   result
